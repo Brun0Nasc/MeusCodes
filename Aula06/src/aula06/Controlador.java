66cd135ee52d2c360796234ebff2ec644b610094
Brun0Nasc <104685758+Brun0Nasc@users.noreply.github.com>
2022-05-08 18:01:40 -0300

    Add files via upload

---
+++ Aula06/src/aula06/Controlador.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula06;
+
+/**
+ *
+ * @author Bruno
+ */
+public interface Controlador {
+    public abstract void ligar();
+    public abstract void desligar();
+    public abstract void abrirMenu();
+    public abstract void fecharMenu();
+    public abstract void maisVolume();
+    public abstract void menosVolume();
+    public abstract void ligarMundo();
+    public abstract void desligarMudo();
+    public abstract void play();
+    public abstract void pause();
+}
---
+++ Aula06/src/aula06/ControleRemoto.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula06;
+
+/**
+ *
+ * @author Bruno
+ */
+public class ControleRemoto implements Controlador {
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+    public ControleRemoto() {
+        this.volume = 50;
+        this.ligado = false;
+        this.tocando = false;
+    }
+    
+    private int getVolume() {
+        return volume;
+    }
+
+    private void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    private boolean getLigado() {
+        return ligado;
+    }
+
+    private void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    private boolean getTocando() {
+        return tocando;
+    }
+
+    private void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+    
+    // Métodos abstratos
+
+    @Override
+    public void ligar() {
+        this.setLigado(true);
+    }
+
+    @Override
+    public void desligar() {
+        this.setLigado(false);
+    }
+
+    @Override
+    public void abrirMenu() {
+        System.out.println("Está ligado? " + this.getLigado());
+        System.out.println("Está tocando? " + this.getTocando());
+    }
+
+    @Override
+    public void fecharMenu() {
+    }
+
+    @Override
+    public void maisVolume() {
+    }
+
+    @Override
+    public void menosVolume() {
+    }
+
+    @Override
+    public void ligarMundo() {
+    }
+
+    @Override
+    public void desligarMudo() {
+    }
+
+    @Override
+    public void play() {
+    }
+
+    @Override
+    public void pause() {
+    }
+    
+    
+}
