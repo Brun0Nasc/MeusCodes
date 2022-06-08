36afa983535d7ac437c1135fc10d27a0ed930b04
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-06-08 19:54:29 -0300

    Update Pessoa.java

--- Aula10/src/aula10/Pessoa.java
+++ Aula10/src/aula10/Pessoa.java
@@ -1,39 +1,34 @@
 package aula10;
-public class Pessoa {
-    private String nome;
-    private int idade;
-    private String sexo;
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
+public class Aula10 {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa();
+        Aluno p2 = new Aluno();
+        Professor p3 = new Professor();
+        Funcionario p4 = new Funcionario();
+        
+        p1.setNome("Pedro");
+        p2.setNome("Maria");
+        p3.setNome("Cláudio");
+        p4.setNome("Fabiana");
+        
+        p2.setCurso("Informática");
+        
+        p3.setSalario(2500.75);
+        
+        p4.setSetor("Estoque");
+        
+        p1.setSexo("M");
+        p2.setSexo("F");
+        p3.setSexo("M");
+        p4.setSexo("F");
+        
+        p2.setIdade(18);
+        
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+        System.out.println(p4.toString());
+        
     }
     
-    public void fazerAniv(){
-        this.setIdade(this.getIdade()+1);
-    }
-
-    @Override
-    public String toString() {
-        return "Pessoa{" + "nome=" + nome + ", idade=" + idade + ", sexo=" + sexo + '}';
-    }
 }
